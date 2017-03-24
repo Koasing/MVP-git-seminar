760fe2ccb3f7af4681a14231b99783675ccc49d6
Koasing <koasing@gmail.com>
2017-03-24 21:34:12 +0900

    Add Class1

---
+++ class1.cpp
@@ -0,0 +1,13 @@
+class class1
+{
+private:
+    int member1;
+    int member2;
+    char *name;
+
+public:
+    void public_member_ftn1();
+    void public_member_ftn2();
+    void public_member_ftn3();
+
+};
