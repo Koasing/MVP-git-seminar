edb4e9e84182610b6b1da8be609a015cb59d8e12
Koasing <koasing@gmail.com>
2017-03-24 20:47:27 +0900

    Add Feature2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+int function2(int worker)
+{
+    int retval;
+
+    // something additional works
+    retval = worker * worker;
+    
+    return retval;
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello World!" << std::endl;
